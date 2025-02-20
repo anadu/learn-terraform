048e10daa93b164fb3179f694ea5b19cf166f634
manoj <ankireddy828@gmail.com>
2025-02-20 19:56:16 +0530

    data source

--- data-source.tf
+++ data-source.tf
@@ -2,3 +2,11 @@ provider "azurerm" {
   features {}
   subscription_id = "68822ef6-c7c9-4c26-8330-5662dc85921f"
 }
+
+data "azurerm_resource_group" "example" {
+  name = "terraform"
+}
+
+output "resource_group" {
+   value = data.azurerm_resource_group.example
+}
