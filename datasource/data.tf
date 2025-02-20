26ef5ec39806b42580a4a30bbe478b209f47070f
manoj <ankireddy828@gmail.com>
2025-02-20 20:35:21 +0530

    datasource

--- datasource/data.tf
+++ datasource/data.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
   subscription_id = "68822ef6-c7c9-4c26-8330-5662dc85921f"
 }
-
+// to retriew the data from azure console
 data "azurerm_resource_group" "example" {
   name = "terraform"
 }
