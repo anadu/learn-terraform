c5c6f541727cd598e272a549ceecf72391de26ff
manoj <ankireddy828@gmail.com>
2025-02-21 11:03:29 +0530

    variables

--- variables/variable.tf
+++ variables/variable.tf
@@ -5,3 +5,22 @@
  output "variabl1"{
     value = var.a1
  }
+
+ variable "a2" {
+   default = 10
+
+ }
+
+ variable "boolean" {
+    default = true
+ }
+
+#string requires only quotes ,that to be double quotes .there is no single quote
+
+output "a2" {
+  value = var.a2
+}
+
+output "boolean" {
+  value = var.boolean
+}
