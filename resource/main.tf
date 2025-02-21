dd9b2130f0050cff45d5fd61f1e57b3127674927
manoj <ankireddy828@gmail.com>
2025-02-21 10:43:05 +0530

    vm

--- resource/main.tf
+++ resource/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = data.azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
