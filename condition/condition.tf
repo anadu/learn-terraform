5e7442596ddf84f903b4ea52fc63ef8f1dfc4a08
manoj <ankireddy828@gmail.com>
2025-02-25 12:06:59 +0530

    commit

--- condition/condition.tf
+++ condition/condition.tf
@@ -3,3 +3,7 @@ variable "a"{}
 output "condition" {
   value = var.a < 10 ? "a is greater than 20" : "a is less than 10"
 }
+
+resource "null_resource" "resource"{
+  count = var.a = 10 ? 1 : 0  
+}
