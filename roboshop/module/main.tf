5b9b03c4cbb1460a9430d84d772ff0d0e45d8287
manoj <ankireddy828@gmail.com>
2025-02-26 12:44:26 +0530

    message

--- roboshop/module/main.tf
+++ roboshop/module/main.tf
@@ -61,14 +61,16 @@ resource "azurerm_virtual_machine" "main" {
   resource_group_name = data.azurerm_resource_group.example.name
   location            = data.azurerm_resource_group.example.location
   network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = var.vmsize
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
    delete_os_disk_on_termination = true
-
-  storage_image_reference {
-    id = "/subscriptions/7b6c642c-6e46-418f-b715-e01b2f871413/resourceGroups/trail1/providers/Microsoft.Compute/galleries/LDOTrail/images/rhel9-devops-practice/versions/04.12.2024"
-  } 
+ storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
   storage_os_disk {
     name              = var.component
     caching           = "ReadWrite"
