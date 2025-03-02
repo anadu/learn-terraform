e9cfc2d7eb97ff5acdbc3aabf4ab2f650f9a4b20
manoj <ankireddy828@gmail.com>
2025-03-02 08:48:33 +0530

    vault

---
+++ vault/vault.tf
@@ -0,0 +1,13 @@
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  address = "https://172.174.242.161:8200"
+}
+
+data "vault_kv_secret" "secret_data" {
+  path = /test/data/demo-ssh
+}
