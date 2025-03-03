c9302522ba40252897bbc3948c43d7ca73d139ef
manoj <ankireddy828@gmail.com>
2025-03-03 10:09:28 +0530

    push

---
+++ misc/vault.tf
@@ -0,0 +1,21 @@
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  address = "http://20.124.84.177:8200"
+  token   = var.token
+}
+
+resource "vault_generic_secret" "example" {
+  path = "secret/foo"
+
+  data_json = <<EOT
+{
+  "foo":   "bar",
+  "pizza": "cheese"
+}
+EOT
+}
